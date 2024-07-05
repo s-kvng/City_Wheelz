832786ecf2e46e80cbd08d9f490d954e98095a9d
s-kvng <kvngnathan8420@gmail.com>
2024-07-05 12:59:25 +0000

    UI: home page layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import Navbar from "@/components/shared/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +24,12 @@ export default function RootLayout({
        <head>
         <ColorSchemeScript />
       </head>
-      <body className={inter.className}> <MantineProvider>{children}</MantineProvider></body>
+      <body className={inter.className}>
+        <MantineProvider>
+          <Navbar/>
+          {children}
+        </MantineProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import { Button } from '@mantine/core';
 import classes from "./page.module.css"
+import Landing from "@/features/landing";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className=" text-xl font-bold text-red-600">
-        Hey there
-      </div>
-      <Button className={classes.btn}>Hi</Button>
+    <main className="">
+     <Landing/>
     </main>
   );
 }
