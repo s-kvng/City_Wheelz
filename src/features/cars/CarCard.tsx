96dc11092f15959a639a6120043478695be1096c
s-kvng <kvngnathan8420@gmail.com>
2024-07-12 22:04:20 +0000

    ui: add cars page
    Implement layout

---
+++ src/features/cars/CarCard.tsx
@@ -0,0 +1,20 @@
+import { Card, Flex, Box, Title, Text } from '@mantine/core'
+import React from 'react'
+
+const CarCard = () => {
+  return (
+    <Card>
+        <Flex>
+            <Box>
+                <Title>Toyota Camry 2023</Title>
+                <Text c="gray.6">Sedan</Text>
+            </Box>
+        </Flex>
+        <Flex>
+            
+        </Flex>
+    </Card>
+  )
+}
+
+export default CarCard
---
+++ src/features/cars/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Box , Card, Container, Divider, Flex, Title } from '@mantine/core';
+import classes from './Style.module.css'
+import SearchEngine from '@/components/shared/SearchEngine';
+
+const CarsLayout = () => {
+  return (
+    <Container className={ classes.parentContainer} size={"xl"} my={"sm"} py="md">
+        <SearchEngine/>
+        {/* Search engine */}
+
+        <Flex direction={{ base : 'column' , md: 'row'}} className={classes.container}>
+            {/* Filters */}
+
+            <Card w={{ base : '100%', md: '350px'}}>
+
+                <Flex align={"center"} justify={"space-between"}>
+                    <Title order={4}>Filters</Title>
+                    <Box display={{ base: "none", md: 'inline-block'}}>
+                        Reset Filters Button
+                    </Box>
+                </Flex>
+                <Box display={{ base: 'none', md: 'block'}}>
+                    Filter Components
+                </Box>
+            </Card>
+
+        </Flex>
+    </Container>
+  )
+}
+
+export default CarsLayout
