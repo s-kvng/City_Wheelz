bcbe180d5ee23fae93a28989c4a751a6164a6b49
s-kvng <kvngnathan8420@gmail.com>
2024-08-13 13:31:25 +0000

    ui: add and implement review modal

---
+++ src/components/forms/Review.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import {
+  PaperProps,
+  Box,
+  Button,
+  Group,
+  LoadingOverlay,
+  Paper,
+  Stack,
+  TextInput,
+  Text,
+  FileInput,
+  rem,
+  Textarea,
+} from "@mantine/core";
+import { useState } from "react";
+
+import { useCreateReviewForm } from "@/hooks/useCreateReviewForm";
+import classes from "./Style.module.css";
+import { IconPhotoScan } from "@tabler/icons-react";
+import supabase from "@/config/superBaseClient";
+import { v4 as uuidV4 } from "uuid";
+
+interface ReviewProps extends PaperProps {
+  driverId: string;
+}
+
+const Review = (props: ReviewProps) => {
+  const icon = (
+    <IconPhotoScan style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+  );
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [imageValue, setImageValue] = useState<File | null>(null);
+
+  const reviewForm = useCreateReviewForm();
+
+  const handleSignup = async () => {
+    const { name, content } = reviewForm.values;
+
+    console.log("submitting");
+
+    setIsSubmitting(true);
+    try {
+      const { data, error } = await supabase
+        .from("Reviews")
+        .insert([
+          {
+            name: name,
+            content: content,
+            driver: props.driverId,
+          },
+        ])
+        .select();
+
+      if (data) {
+        setIsSubmitted(true);
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+      setIsSubmitted(false);
+      reviewForm.reset();
+    }
+  };
+
+  return (
+    <>
+      <LoadingOverlay
+        visible={isSubmitting}
+        zIndex={1000}
+        overlayProps={{ radius: "sm", blur: 2 }}
+      />
+      <Box px={"md"} py={"xl"}>
+        <Paper className={classes.formPaper} withBorder {...props}>
+          <Text size="lg" fw={500}>
+            Add Review
+          </Text>
+
+          <form
+            className="mt-4"
+            onSubmit={reviewForm.onSubmit(() => handleSignup())}
+          >
+            <Stack>
+              <TextInput
+                required
+                label="Name"
+                placeholder="John Doe"
+                radius={"md"}
+                value={reviewForm.values.name}
+                onChange={(event) =>
+                  reviewForm.setFieldValue("name", event.currentTarget.value)
+                }
+              />
+
+              <Textarea
+                required
+                label="Content"
+                description="Only add a review if you have had an encounter with the driver"
+                placeholder="Review must be true"
+                minRows={2}
+                maxRows={4}
+                value={reviewForm.values.content}
+                onChange={(event) =>
+                  reviewForm.setFieldValue("content", event.currentTarget.value)
+                }
+              />
+            </Stack>
+
+            <Group justify="space-between" mt="xl">
+              <Button type="submit" radius={"xl"}>
+                Submit
+              </Button>
+            </Group>
+          </form>
+        </Paper>
+      </Box>
+    </>
+  );
+};
+
+export default Review;
--- src/components/ui/DriverCard/index.tsx
+++ src/components/ui/DriverCard/index.tsx
@@ -1,39 +1,95 @@
-import { Card, Group , Text , Avatar , Stack, Anchor, Button} from '@mantine/core'
-import { IconDownload, IconUserPlus } from '@tabler/icons-react'
-import React from 'react'
+"use client";
+
+import Review from "@/components/forms/Review";
+import supabase from "@/config/superBaseClient";
+import {
+  Card,
+  Group,
+  Text,
+  Avatar,
+  Stack,
+  Anchor,
+  Button,
+  Modal,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconDownload, IconUserPlus } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 const DriverCard = ({ driver }) => {
+  const [opened, { open, close }] = useDisclosure(false);
+  const [errors, setErrors] = useState(null);
+  const [reviews, setReviews] = useState(null);
+  console.log(driver);
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      //fetch reviews for the driver
+      try {
+        let { data, error } = await supabase
+          .from("Reviews")
+          .select("*")
+          .eq("driver", driver.id);
+
+        if (error) {
+          console.error(error);
+          setErrors("Could not fetch cars");
+          setReviews(null);
+          return; // Abort the function if the request fails.
+        }
+
+        if (data) {
+          setReviews(data);
+          console.log(data);
+        }
+      } catch (error) {}
+    };
+
+    fetchReviews();
+  }, []);
   return (
-    <Card  shadow="sm" padding="lg" radius="md" withBorder>
+    <>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
         <Group>
-            <Avatar src={driver.imageSrc1} radius="xl" />
-            <Stack gap={5}>
-              <Text size="sm" fw={700} style={{ lineHeight: 1 }}>
-              {driver.firstName} { driver.lastName}
-              </Text>
-              <Anchor
-                href="https://twitter.com/mantinedev"
-                c="dimmed"
-                size="xs"
-                style={{ lineHeight: 1 }}
-              >
-                {driver.email}
-              </Anchor>
-            </Stack>
+          <Avatar src={driver.imageSrc1} radius="xl" />
+          <Stack gap={5}>
+            <Text size="sm" fw={700} style={{ lineHeight: 1 }}>
+              {driver.firstName} {driver.lastName}
+            </Text>
+            <Anchor
+              href="https://twitter.com/mantinedev"
+              c="dimmed"
+              size="xs"
+              style={{ lineHeight: 1 }}
+            >
+              {driver.email}
+            </Anchor>
+          </Stack>
         </Group>
 
-          <Text size="sm" mt="md">
-            Our Dependable Driver
+        <Text size="sm" mt="md">
+          Our Dependable Driver
+        </Text>
+
+        <Group mt="md" gap="xl">
+          <Text size="sm">
+            <b> {reviews ? reviews.length : 0} </b> Reviews
           </Text>
+          <Button onClick={open} rightSection={<IconUserPlus size={14} />}>
+            Add Review
+          </Button>
 
-          <Group mt="md" gap="xl">
-            <Text size="sm">
-              <b>0</b> Reviews
-            </Text>
-            <Button rightSection={<IconUserPlus size={14} />}>Add Review</Button>
-          </Group>
-    </Card>
-  )
-}
+          <Button onClick={open} rightSection={<IconUserPlus size={14} />}>
+            View Review
+          </Button>
+        </Group>
+      </Card>
 
-export default DriverCard
+      <Modal opened={opened} onClose={close} title="Add Review">
+        <Review driverId={driver.id} />
+      </Modal>
+    </>
+  );
+};
+
+export default DriverCard;
