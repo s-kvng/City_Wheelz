5b48c70836bd57cf9f4fcda112173e12e99eaccb
s-kvng <kvngnathan8420@gmail.com>
2024-08-16 05:30:47 +0000

    ui: minor updates

--- src/app/(Admin)/admin/dashboard/page.tsx
+++ src/app/(Admin)/admin/dashboard/page.tsx
@@ -1,49 +1,49 @@
-"use client"
+"use client";
 
-import DashboardCard from '@/components/cards/DashboardCard'
-import DividerUi from '@/components/ui/DividerUi'
-import { Divider, Grid, Text } from '@mantine/core'
-import React from 'react'
-
+import DashboardCard from "@/components/cards/DashboardCard";
+import DividerUi from "@/components/ui/DividerUi";
+import { Divider, Grid, Text } from "@mantine/core";
+import React from "react";
 
 const list = [
-    {
-        title: 'Cars In Stock',
-        value: 40,
-        icon: 'LineChartIcon',
-        color: 'primary',
-    }
-]
+  {
+    title: "Cars In Stock",
+    value: 40,
+    icon: "LineChartIcon",
+    color: "primary",
+  },
+  {
+    title: "Drivers",
+    value: 10,
+    icon: "LineChartIcon",
+    color: "primary",
+  },
+  {
+    title: "Total Bookings",
+    value: 20,
+    icon: "LineChartIcon",
+    color: "primary",
+  },
+];
 
 const Dashboard = () => {
   return (
     <>
-        <Text mb={"1rem"} size='lg' fw={500}>
-            Welcome Admin
-        </Text>
+      <Text mb={"1rem"} size="lg" fw={500}>
+        Welcome Admin
+      </Text>
 
-        <DividerUi  label="Display Data"/>
+      <DividerUi label="Display Data" />
 
-        <Grid  justify="space-around" px={"md"}>
-        <Grid.Col span={6}>
-            <DashboardCard/>
-        </Grid.Col>
-
-        <Grid.Col span={6}>
-            <DashboardCard/>
-        </Grid.Col>
-
-        <Grid.Col span={6}>
-            <DashboardCard/>
-        </Grid.Col>
-
-        <Grid.Col span={6}>
-            <DashboardCard/>
-        </Grid.Col>
-
-    </Grid>
+      <Grid justify="space-around" px={"md"}>
+        {list.map((item) => (
+          <Grid.Col key={item.title} span={6}>
+            <DashboardCard {...item} />
+          </Grid.Col>
+        ))}
+      </Grid>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/features/Admin/AdminLayout.tsx
+++ src/features/Admin/AdminLayout.tsx
@@ -1,77 +1,84 @@
-"use client"
+"use client";
 
-import SideNavLinks from '@/components/shared/SideNav/SideNavLinks';
-import { AppShell, Burger, Group, Skeleton, Text } from '@mantine/core';
-import { IconGauge, IconSettingsCheck, IconUsersPlus, 
-    IconUsersGroup, IconCar, IconCarGarage, IconClipboardList } from '@tabler/icons-react';
+import SideNavLinks from "@/components/shared/SideNav/SideNavLinks";
+import { AppShell, Burger, Group, Skeleton, Text } from "@mantine/core";
+import {
+  IconGauge,
+  IconSettingsCheck,
+  IconUsersPlus,
+  IconUsersGroup,
+  IconCar,
+  IconCarGarage,
+  IconClipboardList,
+} from "@tabler/icons-react";
 // import { useDisclosure } from '@mantine/hooks';
 // import { MantineLogo } from '@mantinex/mantine-logo';
 
-
 const links = [
-    {
-        label: 'Dashboard',
-        href: '/admin/dashboard',
-        icon: <IconGauge size="1rem" stroke={1.5} />,
-        exact: true,
-      },
-    {
-        label: 'New Driver',
-        href: '/admin/add/driver',
-        icon: <IconUsersPlus size="1rem" stroke={1.5}/>,
-        exact: false,
-      },
-      {
-        label: 'Manage Drivers',
-        href: '/admin/drivers',
-        icon: <IconUsersGroup size="1rem" stroke={1.5}/>,
-        exact: false,
-      },
-      {
-        label: 'Add Car',
-        href: '/admin/add/car',
-        icon: <IconCar size={"1rem"} stroke={1.5} />,
-        exact: false,
-      },
-      {
-        label: 'Manage Cars',
-        href: '/admin/cars',
-        icon: <IconCarGarage size={"1rem"} stroke={1.5} />,
-        exact: false,
-      },
-      {
-        label: 'New Booking',
-        href: '/admin/add/booking',
-        icon: <IconClipboardList size={"1rem"} stroke={1.5} /> ,
-        exact: false,
-      },
-      {
-        label: 'Booking Cards',
-        href: '/admin/bookings',
-        icon: <IconClipboardList size={"1rem"} stroke={1.5} /> ,
-        exact: false,
-      },
-      {
-        label: 'Settings',
-        href: '/admin/settings',
-        icon: <IconSettingsCheck  size={"1rem"} stroke={1.5}/>,
-        exact: false,
-      }
-    
-]
+  {
+    label: "Dashboard",
+    href: "/admin/dashboard",
+    icon: <IconGauge size="1rem" stroke={1.5} />,
+    exact: true,
+  },
+  {
+    label: "New Driver",
+    href: "/admin/add/driver",
+    icon: <IconUsersPlus size="1rem" stroke={1.5} />,
+    exact: false,
+  },
+  {
+    label: "Manage Drivers",
+    href: "/admin/drivers",
+    icon: <IconUsersGroup size="1rem" stroke={1.5} />,
+    exact: false,
+  },
+  {
+    label: "Add Car",
+    href: "/admin/add/car",
+    icon: <IconCar size={"1rem"} stroke={1.5} />,
+    exact: false,
+  },
+  {
+    label: "Manage Cars",
+    href: "/admin/cars",
+    icon: <IconCarGarage size={"1rem"} stroke={1.5} />,
+    exact: false,
+  },
+  {
+    label: "New Booking",
+    href: "/admin/add/booking",
+    icon: <IconClipboardList size={"1rem"} stroke={1.5} />,
+    exact: false,
+  },
+  {
+    label: "Booking Cards",
+    href: "/admin/bookings",
+    icon: <IconClipboardList size={"1rem"} stroke={1.5} />,
+    exact: false,
+  },
+  // {
+  //   label: 'Settings',
+  //   href: '/admin/settings',
+  //   icon: <IconSettingsCheck  size={"1rem"} stroke={1.5}/>,
+  //   exact: false,
+  // }
+];
 
-
-
-export function AdminLayout({ children}) {
-//   const [opened, { toggle }] = useDisclosure();
+export function AdminLayout({ children }) {
+  //   const [opened, { toggle }] = useDisclosure();
 
   return (
     <AppShell
       layout="alt"
       header={{ height: 60 }}
       footer={{ height: 60 }}
-      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: false } }}
-      aside={{ width: 100, breakpoint: 'md', collapsed: { desktop: false, mobile: true } }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: false } }}
+      aside={{
+        width: 100,
+        breakpoint: "md",
+        collapsed: { desktop: false, mobile: true },
+      }}
       padding="md"
     >
       <AppShell.Header>
@@ -84,21 +91,21 @@ export function AdminLayout({ children}) {
       <AppShell.Navbar p="md">
         <Group mb={"3rem"}>
           {/* <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" /> */}
-          <Text>Navbar</Text>
+          {/* <Text>Navbar</Text> */}
         </Group>
-          {
-            links.map((link, index)=>(
-               <div key={index}>
-                 <SideNavLinks href={link.href} label={link.label} icon={link.icon}/>
-               </div>
-            ))
-          }
+        {links.map((link, index) => (
+          <div key={index}>
+            <SideNavLinks
+              href={link.href}
+              label={link.label}
+              icon={link.icon}
+            />
+          </div>
+        ))}
       </AppShell.Navbar>
-      <AppShell.Main>
-        { children}
-      </AppShell.Main>
-      <AppShell.Aside p="md">Aside</AppShell.Aside>
-      <AppShell.Footer p="md">Footer</AppShell.Footer>
+      <AppShell.Main>{children}</AppShell.Main>
+      <AppShell.Aside p="md"></AppShell.Aside>
+      <AppShell.Footer p="md">Powered By Supabase</AppShell.Footer>
     </AppShell>
   );
 }
